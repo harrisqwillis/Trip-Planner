3c44697b3aa343528bb4b533d84b2e69a52ac4c7
EE <evelyneidsath@gmail.com>
2023-12-07 20:15:27 -0800

    revise request function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,20 +1,25 @@
 var city = /* document.getElementById(#); */ 'Paris';
-var tripStart = /* document.getElementById(#); */ '2023-01-01';
+var tripStart = /* document.getElementById(#); */ '2024-01-01';
 var main = document.querySelector(".main");
 
 function getApi() {
     var requestUrl = 'https://api.weatherapi.com/v1/future.json?key=ffd028c4f113423dad624715230812&q=' + city + '&dt=' + tripStart;
   
-    fetch(requestUrl)
-      .then(function (response) {
-        console.log(response);
-        main.appendChild(response);
-        return response.json();
+    fetch(requestUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayWeather(data);
+            })
+        }
+            console.log(response);
+            /* return response.json(); */
       })
   }
 
 getApi();
 
 function displayWeather() {
+    var weatherContainer = document.createElement('div');
+    weatherContainer.classList = 'enter tailwind classes';
 
 }
