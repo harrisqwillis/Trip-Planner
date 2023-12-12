17fa29427f4989a30f3404606e71ca1144b30005
Ryan Gurley <gurleyryan@gmail.com>
2023-12-11 20:23:28 -0800

    Started TripAdvisor API code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,7 +8,7 @@ var weatherDisplay = document.getElementById("weather-display");
 
 function getApi() {
     var requestUrl = 'https://api.weatherapi.com/v1/future.json?key=ffd028c4f113423dad624715230812&q=' + city + '&dt=' + tripStart;
-  
+
     fetch(requestUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
@@ -20,8 +20,9 @@ function getApi() {
                 console.log(data);
             })
         }
-      })
-  }
+    })
+};
+
 getApi();
 
 function displayWeather(future) {
@@ -29,5 +30,27 @@ function displayWeather(future) {
     weatherDisplay.appendChild(forecastItem);
     forecastItem.classList = 'enter tailwind classes';
     forecastItem.innerHTML = future;
-}
+};
+
 //for the weather values we could make an outline in html that has boxes with text saying "max temp", "average temp", etc and then append each forecast item to its relevant container
+
+function getAdvisor() {
+    var requestUrl = 'https://api.content.tripadvisor.com/api/v1/location/search?key=B023F0317F4C4F7FABFEB3113D8B5FD2&searchQuery=' + city + '&language=en';
+    var options = { method: 'GET', headers: { accept: 'application/json' } };
+
+    fetch(requestUrl, options).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            })
+        }
+    })
+};
+
+getAdvisor();
+
+function displayAdvisor() {
+
+};
+
+displayAdvisor();
